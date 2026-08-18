839b5d4ef11ff2cc53d2ef8a2e5dd7b6b23d97da
risan14 <64809915+risan14@users.noreply.github.com>
2026-08-18 12:43:26 +0600

    Update content.js

--- js/content.js
+++ js/content.js
@@ -18,7 +18,7 @@ const SITE_CONTENT = {
     eyebrow: "Independent designer · Available for selected projects",
     title: "I create <em>visual experiences</em> that people remember.",
     description: "Graphic design, brand communication and thoughtful WordPress websites for organizations, businesses and people with something worth saying.",
-    image: "images/hero.jFIF",
+    image: "images/hero.JFIF",
     note: "Currently accepting new projects"
   },
 
