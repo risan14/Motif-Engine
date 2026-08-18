4c7075367be564495649b076a1975637d98d7f4a
risan14 <64809915+risan14@users.noreply.github.com>
2026-08-18 15:20:25 +0600

    Update content.js

--- js/content.js
+++ js/content.js
@@ -97,7 +97,7 @@ const SITE_CONTENT = {
       categories: ["All", "Campaign", "Digital"],
       year: "2026",
       client: "Humanitarian organization",
-      image: "images/project-03.jpg",
+      image: "images/Humanitarian Campaign.webp",
       description: "A digital campaign concept using illustration and strong visual storytelling to communicate humanitarian values.",
       details: ["Campaign concept", "Social media", "Illustration direction"],
       link: "#"
